490c2cc26af154300b6beb614780f54eb47f2dc1
KarinMeijvogel <57796369+KarinMeijvogel@users.noreply.github.com>
2019-11-22 18:56:39 +0100

    Add files via upload

---
+++ week2/scripts/javascript.js
@@ -0,0 +1,14 @@
+var hamburgerButton = document.getElementsByTagName('button')[1];
+var closeMenuButton = document.getElementsByTagName('button')[0];
+var sliderMenu = document.getElementById('overlaymenu');
+
+function openMenu() {
+    sliderMenu.style.right = '0';
+}
+function closeMenu() {
+    sliderMenu.style.right = '-300px';
+}
+
+hamburgerButton.addEventListener('click', openMenu);
+closeMenuButton.addEventListener('click', closeMenu);
+
